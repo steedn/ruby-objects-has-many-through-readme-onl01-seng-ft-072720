73c45f1a8999640944c6ce99cd5942369b3cb63a
Nathan Steed <steedn@outlook.com>
2020-08-16 05:37:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,4 +11,10 @@ class Waiter
   def new_meal(customer, total, tip)
     Meal.new(self, customer, total, tip)
   end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
 end
