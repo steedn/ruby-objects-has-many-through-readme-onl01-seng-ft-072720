0c315176e73a5151035be4c3fd957fe46e595be8
Nathan Steed <steedn@outlook.com>
2020-08-16 05:43:12 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
-  
+
+  @@all = []
+  def initialize(name, age)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,15 @@
 class Meal
+  @@all = []
+  attr_accessor :customer, :waiter, :total, :tip
+  def initialize(waiter, customer, total, tip)
+    @@all << self
+    @customer = customer
+    @waiter = waiter
+    @total = total
+    @tip = tip
+  end
 
+  def self.all
+    @@all
+  end
 end
